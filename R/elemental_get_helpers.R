56b5df0c733959cfe59e0d42ec95122cac4d52a9
Don-Li <yli877@aucklanduni.ac.nz>
2017-02-20 11:55:48 +1300

    Updated namespace Added functions to change behaviours in EBD population

--- R/elemental_get_helpers.R
+++ R/elemental_get_helpers.R
@@ -2,7 +2,7 @@
 
 #' Elemental accessor helper functions
 #'
-#' An accessor function is a function that retrieves variables from an object. Because most of the \code{CAB} package is programmed in an object-oriented style, accessor functions are used in almost every part of the package. Hence, these accessor functions will be documented.
+#' An accessor function is a function that retrieves variables from an object. Because most of the \code{CAB} package is programmed in an object-oriented style, accessor functions are used in almost every part of the package. Hence, these accessor functions will be documented here.
 #'
 #'
 #' \describe{
@@ -12,6 +12,8 @@
 #'     \item{\code{get.resps( resp_input )}}{Takes the vector \code{resp_input} and returns the number of elements that are not \code{NAN}.}
 #'     \item{\code{get.time( time_input )}}{Takes the vector \code{time_input} and returns a last value that is not \code{NAN}. If all values are \code{NAN}, returns 0.}
 #'     \item{\code{get.IRI_responses( resp_times, rft_times )}}{Takes a list in the \code{input} object that tracks the response times within an inter-reinforcement interval. Otherwise, takes a numeric.}
+#'     \item{\code{bin2int}}{Takes a matrix of binary strings with each column corresponding to each string and the rows corresponding to the digits of each binary string.}
+#'     \item{\code{int2bin}}{Takes a vector of integers with an argument \code{digits} and converts each integer into a corresponding binary string with length of \code{digits}.}
 #' }
 #'
 #' @seealso
@@ -30,7 +32,6 @@ e_get.name = function( elementary_obj ){
 e_get.fx = function( elementary_obj ){
     slot( elementary_obj, class( elementary_obj ) )
 }
-
 
 #' @rdname accessor_helpers
 #' @aliases get.reserve
@@ -58,17 +59,6 @@ get.time = function( time_input ) {
     x
 }
 
-# The following function was for getting the responses during an inter-reinforcement interval for the delay-of-reinforcement gradients in Catania's Operant Reserve. Now, we have chosen to directly record the response times within an inter-reinforcement interval. Hence, this function is no longer required.
-# #' @rdname accessor_helpers
-# #' @export get.IRI_responses
-#
-# get.IRI_responses = function( resp_times, rft_times ){
-#     IRI_rft_times = tail( rft_times[ !is.nan( rft_times )], 2)
-#     IRI_resp_times = resp_times[ !is.nan(resp_times) ]
-#     if ( length( IRI_rft_times ) == 1 ) IRI_rft_times = c( 0, IRI_rft_times )
-#     IRI_resp_times[ IRI_resp_times <= IRI_rft_times[2] & IRI_resp_times >= IRI_rft_times[1] ] - IRI_rft_times[1]
-# }
-
 #' @rdname accessor_helpers
 #' @aliases get.IRI_resp_times
 #' @export get.IRI_resp_times
@@ -76,3 +66,18 @@ get.IRI_resp_times = function( iri_resp_times ){
     if ( is.list( iri_resp_times ) ) iri_resp_times$data[ iri_resp_times$counts ] - iri_resp_times$data[ 1: iri_resp_times$counts ]
     else iri_resp_times
 }
+
+#' @rdname accessor_helpers
+#' @aliases bin2int
+#' @export bin2int
+bin2int = function( binaries ){
+    drop( crossprod( binaries, 2^(0:(nrow(binaries)-1)) ) )
+}
+
+#' @rdname accessor_helpers
+#' @aliases int2bin
+#' @export int2bin
+int2bin = function( integers, digits ){
+    vapply( integers, function(x) as.integer( intToBits(x)[1:digits] ), FUN.VALUE = 1:digits)
+}
+
